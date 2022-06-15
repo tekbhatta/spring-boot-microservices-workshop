55dbc6ab3a408be932a2660de5e9d222c7ed844a
Tek <tekbhatta@gmail.com>
2022-06-14 23:21:19 -0500

    tek commit

--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/models/Movie.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/models/Movie.java
@@ -5,6 +5,9 @@ public class Movie {
     private String name;
     private String description;
 
+    private String test55;
+
+
     public Movie(String movieId, String name, String description) {
         this.movieId = movieId;
         this.name = name;
